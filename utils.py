85fee527723f88228c2ac0d97233cbfdda857b89
mkonopacka <67000987+mkonopacka@users.noreply.github.com>
2022-07-18 21:14:30 +0200

    Changed something

--- utils.py
+++ utils.py
@@ -1,7 +1,10 @@
-from xml.dom import ValidationErr
 import numpy as np 
+import pandas as pd
 
 def accuracy(true: np.ndarray, pred: np.ndarray) -> float:
     if true.shape != pred.shape:
         raise Exception("Arrays must have equal shapes.")
     return np.mean(true == pred)
+
+def find_colnames_with(df: pd.DataFrame, what: str = "pred") -> list[str]:
+    return [col for col in df if what in col]
