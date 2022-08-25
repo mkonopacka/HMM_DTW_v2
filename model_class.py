151f685158625ce64a2e4706e25626f2a75e1433
mkonopacka <67000987+mkonopacka@users.noreply.github.com>
2022-08-25 14:31:40 +0200

    ... add new data

--- model_class.py
+++ model_class.py
@@ -1,4 +1,5 @@
 from ARIMA import *
+from hmmlearn import hmm
 
 class ARIMA_model:
     """Autoregressive integrated moving average model.
@@ -16,6 +17,36 @@ class ARIMA_model:
 
     def sample(self, n):
         return ARIMA(self.phi, self.theta, self.d, self.t, self.mu, self.sigma, n, self.burn)
+
+    @property
+    def p(self):
+        return len(self.phi)
+
+    @property
+    def q(self):
+        return len(self.theta)
+
+class ARIMA_HMM_model:
+    """Mixture of tau*ARIMA and (1-tau)*HMM model."""
+    def __init__(self, phi, theta, d = 0, t = 0, mu = 0, sigma = 1, burn = 10, tau: float = 0.3, HMM_instance: hmm.BaseHMM = None):
+        if HMM_instance is None:
+            raise NotImplementedError(f"must pass HMM instance")
+        
+        self.phi = phi
+        self.theta = theta
+        self.d = d
+        self.t = t
+        self.mu = mu
+        self.sigma = sigma
+        self.burn = burn
+        self.HMM_instance = HMM_instance
+        self.tau = tau
+
+    def sample(self, n):
+        ARIMA_sample = ARIMA(self.phi, self.theta, self.d, self.t, self.mu, self.sigma, n, self.burn)
+        HMM_sample, _ = self.HMM_instance.sample(n)
+        return self.tau*ARIMA_sample + (1-self.tau)*HMM_sample
+
 
     @property
     def p(self):
